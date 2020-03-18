962a81ebf5444d021283a3f685911e36db9df84f
johnathanmika <johnathanmika@users.noreply.github.com>
2020-03-17 20:00:48 -0500

    Fixed UI
    Changed bottom nav bar names and moved around button and spinners. Got rid of frament text.

--- app/src/main/java/com/example/resistorscanner/MainActivity.java
+++ app/src/main/java/com/example/resistorscanner/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_camera, R.id.navigation_history)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
