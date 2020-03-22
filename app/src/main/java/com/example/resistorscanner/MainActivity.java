44d1a9fe7d98480c9dae16330e9f7f9c4a35f939
Brendan Nenninger <brendannenn@gmail.com>
2020-03-22 14:27:05 -0500

    Comments MainActivity, removes MainActivityImpl

--- app/src/main/java/com/example/resistorscanner/MainActivity.java
+++ app/src/main/java/com/example/resistorscanner/MainActivity.java
@@ -1,11 +1,6 @@
 package com.example.resistorscanner;
 
-import android.graphics.Color;
 import android.os.Bundle;
-
-import android.util.Log;
-//import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -14,28 +9,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import org.opencv.android.OpenCVLoader;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
-import static android.hardware.Camera.open;
-//import androidx.navigation.NavController;
-//import androidx.navigation.Navigation;
-//import androidx.navigation.ui.AppBarConfiguration;
-//import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
 
     Spinner spinner1, spinner2, spinner3, spinner4;
 
+    // TextView for displaying the result
     TextView display;
 
     private static final String TAG = "MainActivity";
@@ -55,39 +44,42 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.colors, android.R.layout.simple_spinner_item);
-//        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //sets up ArrayAdapters to load the different types of band colors to the spinners
         ArrayAdapter<ColorValue> valueColors = new ArrayAdapter<ColorValue>(this, android.R.layout.simple_spinner_dropdown_item, ColorValue.getValueColors());
+        ArrayAdapter<ColorValue> exponentColors = new ArrayAdapter<ColorValue>(this, android.R.layout.simple_spinner_dropdown_item, ColorValue.getExponentColors());
+        ArrayAdapter<ColorValue> toleranceColors = new ArrayAdapter<ColorValue>(this, android.R.layout.simple_spinner_dropdown_item, ColorValue.getToleranceColors());
+
+        // initializes the four spinners
+        // the first two are for value bands, the third is for multiplier band, the fourth is for
+        // tolerance band
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
         spinner1.setAdapter(valueColors);
         spinner1.setOnItemSelectedListener(this);
 
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         spinner2.setAdapter(valueColors);
         spinner2.setOnItemSelectedListener(this);
-
+        
         spinner3 = (Spinner) findViewById(R.id.spinner3);
-        ArrayAdapter<ColorValue> exponentColors = new ArrayAdapter<ColorValue>(this, android.R.layout.simple_spinner_dropdown_item, ColorValue.getExponentColors());
         spinner3.setAdapter(exponentColors);
         spinner3.setOnItemSelectedListener(this);
 
         spinner4 = (Spinner) findViewById(R.id.spinner4);
-        ArrayAdapter<ColorValue> toleranceColors = new ArrayAdapter<ColorValue>(this, android.R.layout.simple_spinner_dropdown_item, ColorValue.getToleranceColors());
         spinner4.setAdapter(toleranceColors);
         spinner4.setOnItemSelectedListener(this);
 
-
-        display = (TextView)findViewById(R.id.display);
+        // initializes the textview for displaying results
+        display = (TextView) findViewById(R.id.display);
         display.setTextSize(25);
-
-
     }
 
+    //method called every time an item is selected from one of the spinners
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //makes the color value display temporarily at the bottom of the screen
         String text = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-
+        //updates the output field
         convert();
     }
 
@@ -96,18 +88,24 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
 
     }
 
-    public void convert(View view)
-    {
+    //action that activates when the button is pressed
+    //TODO make this save to history, as conversion happens automatically
+    public void convert(View view) {
         convert();
     }
 
-    public void convert(){
+    /**
+     * Converts the colors to a resistor value and outputs the result to the text field
+     */
+    public void convert() {
+        //reads the value of the color bands into an arraylist
         ArrayList<ColorValue> bands = new ArrayList<>();
         bands.add(ColorValue.valueOf(spinner1.getSelectedItem().toString().toUpperCase()));
         bands.add(ColorValue.valueOf(spinner2.getSelectedItem().toString().toUpperCase()));
         bands.add(ColorValue.valueOf(spinner3.getSelectedItem().toString().toUpperCase()));
         bands.add(ColorValue.valueOf(spinner4.getSelectedItem().toString().toUpperCase()));
 
+        //creates a colorStorage object to convert the value
         ResistorValue colorStorage = new ResistorValueColorStorage(bands);
         display.setText(colorStorage.outputStringValue());
     }
